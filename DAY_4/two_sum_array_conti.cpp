9b33c22e88a5c39530bc47722b890547cd859bfc
rohanpimparkar26 <rohanpimparkar26@gmail.com>
2025-02-28 17:01:22 +0530

    avc

---
+++ DAY_4/two_sum_array_conti.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int data [100][100];
+void data2(int num[],int size)
+{
+    for (int i = 0;i<size;i++)
+    {
+        for(int j=0;j<size;j++)
+        {
+            data2[nums[i]nums[j]]=i*100+j
+        }
+        
+    }
+}
